03eb51f2920e62cc61c3cfc047fb17bfc09abc7f
Kaiba Corporation <kaiba.corporation.github@gmail.com>
2019-08-13 00:01:00 -0400

    Basic Message Box
    Added a basic message box.

---
+++ Assets/SibylSystem/TDOANE/MessageBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBox : MonoBehaviour {
+
+    public UILabel title;
+    public UILabel message;
+    public UIButton ok;
+
+    void Start () {
+        UIHelper.registEvent(ok, onOk);
+    }
+
+    void onOk ()
+    {
+        Destroy(gameObject);
+    }
+}
